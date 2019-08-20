82c273ea5164e12131f71ac72397dccc2787d133
Masashi Kimura <kimura@convergence-lab.com>
2019-08-21 08:49:33 +0900

    initial commit

---
+++ novograd.py
@@ -0,0 +1,70 @@
+import torch
+from torch import optim
+import math
+
+class NovoGrad(optim.Optimizer):
+    def __init__(self, params, grad_averaging=False, lr=0.01, betas=(0.9, 0.999), eps=1e-8, weight_decay=0):
+        defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
+        super(NovoGrad, self).__init__(params, defaults)
+        self._lr = lr
+        self._beta1 = betas[0]
+        self._beta2 = betas[1]
+        self._eps = eps
+        self._wd = weight_decay
+        self._grad_averaging = grad_averaging
+
+        self._momentum_initialized = False
+
+    def step(self, closure=None):
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        if not self._momentum_initialized:
+            for group in self.param_groups:
+                for p in group['params']:
+                    if p.grad is None:
+                        continue
+                    state = self.state[p]
+                    grad = p.grad.data
+                    if grad.is_sparse:
+                        raise RuntimeError('NovoGrad does not support sparse gradients')
+
+                    v = torch.norm(grad)
+                    state['step'] = 0
+                    state['v'] = v
+                    state['m'] = grad/torch.sqrt(v + self._eps) + self._wd * p.data
+                    state['grad_ema'] = None
+            self._momentum_initialized = True
+        
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                state = self.state[p]
+                state['step'] += 1
+                v, m, grad_ema, step = state['v'], state['m'], state['grad_ema'], state['step']
+
+                grad = p.grad.data     
+
+                g2 = torch.sum(grad**2)
+                grad_ema = g2 if grad_ema is None else grad_ema * \
+                    self._beta2 + g2*(1. - self._beta2)
+                grad *= 1.0 / torch.sqrt(grad_ema + self._eps)
+
+                if self._wd > 0.:
+                    grad += self._wd*p
+                if self._grad_averaging:
+                    grad *= (1. - self._beta1)
+                
+                gn2 = torch.norm(grad)
+                v = self._beta2*v + (1. - self._beta2)*gn2
+                m = self._beta1*m + (grad / torch.sqrt(v + self._eps) + self._wd*p.data)
+         
+                bias_correction1 = 1 - self._beta1 ** step
+                bias_correction2 = 1 - self._beta2 ** step
+                step_size = self._lr * math.sqrt(bias_correction2 + self._eps) / bias_correction1
+                
+                state['v'], state['m'], state['grad_ema'] = v, m, grad_ema
+                p.data.add_(-step_size, m)
+        return loss
---
+++ test_mnist.py
@@ -0,0 +1,91 @@
+import torch.nn.functional as F
+from torch import nn, optim
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+
+from novograd import NovoGrad
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, ), (0.5, ))])
+trainset = torchvision.datasets.MNIST(root='./data',
+                                      train=True,
+                                      download=True,
+                                      transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset,
+                                          batch_size=100,
+                                          shuffle=True,
+                                          num_workers=2)
+
+testset = torchvision.datasets.MNIST(root='./data',
+                                      train=False,
+                                      download=True,
+                                      transform=transform)
+testloader = torch.utils.data.DataLoader(testset,
+                                         batch_size=100,
+                                         shuffle=False,
+                                         num_workers=2)
+
+classes = tuple(np.linspace(0, 9, 10, dtype=np.uint8))
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3)
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.dropout1 = nn.Dropout2d()
+        self.fc1 = nn.Linear(12 * 12 * 64, 128)
+        self.dropout2 = nn.Dropout2d()
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.dropout1(x)
+        x = x.view(-1, 12 * 12 * 64)
+        x = F.relu(self.fc1(x))
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        return x
+
+model = Net()
+criterion = nn.CrossEntropyLoss()
+# optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+optimizer = NovoGrad(model.parameters(), lr=0.01)
+
+
+epochs = 1
+
+for epoch in range(epochs):
+    running_loss = 0.0
+    for i, (inputs, labels) in enumerate(trainloader, 0):
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 100 == 99:
+            print('[{:d}, {:5d}] loss: {:.3f}'.format(epoch + 1, i + 1, running_loss / 100))
+            running_loss = 0.0
+
+print('Finished Training')
+
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for (images, labels) in testloader:
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+print('Accuracy: {:.2f} %%'.format(100 * float(correct/total)))
