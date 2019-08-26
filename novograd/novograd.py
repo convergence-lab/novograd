a131f16159b6a424f29521b7c64e319dddbb6385
Masashi Kimura <kimura@convergence-lab.com>
2019-08-26 09:49:51 +0900

    fix applying grad_ema

--- novograd/novograd.py
+++ novograd/novograd.py
@@ -1,7 +1,6 @@
 # Author Masashi Kimura (Convergence Lab.)
 import torch
 from torch import optim
-import math
 
 class NovoGrad(optim.Optimizer):
     def __init__(self, params, grad_averaging=False, lr=0.1, betas=(0.95, 0.98), eps=1e-8, weight_decay=0):
@@ -35,25 +34,30 @@ class NovoGrad(optim.Optimizer):
                     m = grad/(torch.sqrt(v) + self._eps) + self._wd * p.data
                     state['v'] = v
                     state['m'] = m
+                    state['grad_ema'] = None
             self._momentum_initialized = True
-        
+
         for group in self.param_groups:
             for p in group['params']:
                 if p.grad is None:
                     continue
                 state = self.state[p]
-                v, m = state['v'], state['m']
+                v, m, grad_ema = state['v'], state['m'], state['grad_ema']
 
                 grad = p.grad.data
                 g2 = torch.norm(grad)**2
+                grad_ema = g2 if grad_ema is None else grad_ema * \
+                    self._beta2 + g2*(1. - self._beta2)
+                grad *= 1.0 / (torch.sqrt(grad_ema) + self._eps)
+
                 if self._wd > 0.:
                     grad += self._wd*p
                 if self._grad_averaging:
                     grad *= (1. - self._beta1)
-                
+
                 v = self._beta2*v + (1. - self._beta2)*g2
                 m = self._beta1*m + (grad / (torch.sqrt(v) + self._eps) + self._wd*p.data)
-         
+
                 state['v'], state['m']= v, m
                 p.data.add_(-group['lr'], m)
         return loss
